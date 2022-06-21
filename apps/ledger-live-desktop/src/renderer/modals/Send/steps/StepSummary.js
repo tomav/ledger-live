e9ea61027125b6676922bc38c8d87927d73c4d0d
laure-lebon <laure.lebon-ext@ledger.fr>
2022-06-21 15:07:11 +0200

    LIVE-1127 after run check

--- apps/ledger-live-desktop/src/renderer/modals/Send/steps/StepSummary.js
+++ apps/ledger-live-desktop/src/renderer/modals/Send/steps/StepSummary.js
@@ -138,16 +138,16 @@ export default class StepSummary extends PureComponent<StepProps> {
           </Box>
           <Separator />
           {memo && (
-              <Box horizontal alignItems="center" mb={2}>
-                <Text ff="Inter|Medium" color="palette.text.shade40" fontSize={4}>
-                  <Trans i18nKey="operationDetails.extra.memo" />
+            <Box horizontal alignItems="center" mb={2}>
+              <Text ff="Inter|Medium" color="palette.text.shade40" fontSize={4}>
+                <Trans i18nKey="operationDetails.extra.memo" />
+              </Text>
+              <Ellipsis ml={2}>
+                <Text ff="Inter|Medium" fontSize={4}>
+                  {memo}
                 </Text>
-                <Ellipsis ml={2}>
-                  <Text ff="Inter|Medium" fontSize={4}>
-                    {memo}
-                  </Text>
-                </Ellipsis>
-              </Box>
+              </Ellipsis>
+            </Box>
           )}
           {!isNFTSend ? (
             <Box horizontal justifyContent="space-between" mb={2}>
